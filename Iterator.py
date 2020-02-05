0d211ba945a7d25b226ce3c9e3c0fc9b8018fa66
Stefan Santrac <santrac.stefan@gmail.com>
2020-02-05 16:07:51 +0100

    #parsiranje_skupa_HTML_dokumenata izvrseno parsiranje svih HTML datoteka

--- Iterator.py
+++ Iterator.py
@@ -1,29 +1,42 @@
 from parserReci import Parser
+from Trie import *
 from builtins import print
 import os
 
+def funkcija(inp):
+    #path = 'C:/Users/Korisnik/PycharmProjects/ASPython/test-skup/python-2.7.7-docs-html'
 
-path = 'C:/Users/Korisnik/PycharmProjects/ASPython/test-skup/python-2.7.7-docs-html'
+    path = inp
 
-folder = os.fsencode(path)
+    folder = os.fsencode(path)
 
-filenames = []
-brojac=0
+    filenames = []
+    brojac=0
 
-for file in os.listdir(folder):
-    filename = os.fsdecode(file)
-    if filename.endswith( ('.html') ):
-        filenames.append(filename)
-        brojac=brojac+1
+    parser = Parser()
+    for subdir, dirs, files in os.walk(path):
+        for file in files:
+            filename = os.fsdecode(file)
+            if filename.endswith( ('.html') ):
+                finalPath=(os.path.join(subdir, file))
+                #print(finalPath)
+                filenames.append(filename)
+                brojac=brojac+1
+                parser.parse(finalPath)
+
+
+    print(brojac)
+
+
+    print(parser.words)
 
 
 if __name__ == "__main__":
-    print(brojac)
-    for i in range(0,len(filenames),1):
-        print(filenames[i])
-        parser = Parser()
-        parser.parse("C:/Users/Korisnik/PycharmProjects/ASPython/test-skup/python-2.7.7-docs-html/" + filenames[i])
 
-    print(parser.words)
+    root = TrieNode('*')
+    print("Unesite korenski direktorijum")
+    inp = input()
+    funkcija(inp)
+
 
 
