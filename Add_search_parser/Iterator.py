8e96f34672bd4748455b491f7d6d73457080bf2e
Stefan Santrac <santrac.stefan@gmail.com>
2020-02-23 14:37:06 +0100

    Pisanje komentara

--- Add_search_parser/Iterator.py
+++ Add_search_parser/Iterator.py
@@ -8,35 +8,35 @@ import os
 def dodaj(inp):
     #path = 'C:/Users/Korisnik/PycharmProjects/ASPython/test-skup/python-2.7.7-docs-html'
 
-    path = inp
+    path = inp                  # Unesena vrednost path-a
 
-    brojac = 0
-    root = TrieNode('*')
-    graf = Graph()
+    brojac = 0                  # Brojac za HTML stranice
+    root = TrieNode('*')        # Napravi novi trie
+    graf = Graph()              # Napravi novi graf
     folder = os.fsencode(path)
 
     filenames = []
-    dictGraf = {}
-    rangStranica = {}
-    imenaStranicaSaLinkovima = {}
+    dictGraf = {}               # Recnik koji ce cuvati sve HTML stranice i linkove na njima, koji cemo dodavati u graf
+    rangStranica = {}           # Recnik za odredjivanje ranga (cuva HTML stranice i broj linkova koji pokazuju ka njima)
+    imenaStranicaSaLinkovima = {}   # Recnik za odredjivanje ranga (cuva HTML stranice i sve linkove koji pokazuju ka njoj)
 
     parser = Parser()
 
 
-    for subdir, dirs, files in os.walk(path):
+    for subdir, dirs, files in os.walk(path):                    # Prolazak kroz sve file-ove za zadati korenski direktorijum
         for file in files:
             filename = os.fsdecode(file)
             if filename.endswith('.html'):
 
-                finalPath = (os.path.join(subdir, file))
+                finalPath = (os.path.join(subdir, file))          # Putanja do HTML file-a
                 filenames.append(filename)
-                brojac = brojac + 1
-                parser.parse(finalPath)
-                dictGraf[finalPath] = parser.links
+                brojac = brojac + 1                               # Povecaj vrednost brojaca HTML stranica
+                parser.parse(finalPath)                           # Parsiraj HTML stranicu (celu putanju)
+                dictGraf[finalPath] = parser.links                # Popunjavaj recnik za graf
                 for word in parser.words:
-                    add(root, word.lower(), finalPath,parser.links)
+                    add(root, word.lower(), finalPath,parser.links)     # Svaku rec u parsiranoj stranici dodaj u Trie
 
-    rangStranica, imenaStranicaSaLinkovima = dodavanjeUGraf(graf,dictGraf)
+    rangStranica, imenaStranicaSaLinkovima = dodavanjeUGraf(graf,dictGraf)      #Dodavanje u graf
     #print("Svi html fileovi sa svim linkovima")
     #print(dictGraf)
     print("Ukupan broj HTML stranica: ", brojac)
@@ -50,22 +50,22 @@ def trazi(root, rec):
     dictLinkova = {}
     set = Set()
 
-    resenje = find_prefix(root,rec)
-    if resenje is not None:
-        dictStranica = resenje[2]
+    resenje = find_prefix(root,rec)                       # Pretrazivanje trie-a u zavisnosti od vrednosti parametra rec
+    if resenje is not None:                               # Ukoliko smo uspesno pretrazili trie
+        dictStranica = resenje[2]                         # Recnik sa HTML stranicama i brojem trazene reci na njima
         #print(dictStranica)
         #print("Linkovi na stranicama")
-        dictLinkova = resenje[3]
+        dictLinkova = resenje[3]                          # Recnik sa HTML stranicama i linkovima na njima
         #print(dictLinkova)
         #print("Ukupan broj reci: ", resenje[1])
-        set = resenje[4]
+        set = resenje[4]                                  # Skup HTML stanica koje zadovoljavaju upit
 
 
 
 
         return set, dictLinkova, dictStranica
 
-    return Set(), {}, {}
+    return Set(), {}, {}                                    # Ukoliko nismo uspesno pronasli trazenu rec u trie
 
 
 
--- Rangiranje/DodavanjeGraf.py
+++ Rangiranje/DodavanjeGraf.py
@@ -1,19 +1,19 @@
 def dodavanjeUGraf(graph, dict):
     rangStranica = {}
-    imenaStranicaSaLinkovima = {} #imena linkova sa stranicama
-    for file in dict:   #prolazi kroz sve html stranice u direktorijumu
-        graph.add_vertex(file) #dodaje ih kao cvorove
-        for link in dict[file]:  #prolazi kroz sve linkove u direktorijumu
-            graph.add_edge({file, link})  #dodaje ih kao grane
-            if link in rangStranica:  #racuna koliko ce linkova imati neka html stranica
-                rangStranica[link]+=1  #plus 1 za rang ako se link vec nalazi u rangStranica
+    imenaStranicaSaLinkovima = {}           #imena linkova sa stranicama
+    for file in dict:                       #prolazi kroz sve html stranice u direktorijumu
+        graph.add_vertex(file)              #dodaje ih kao cvorove
+        for link in dict[file]:             #prolazi kroz sve linkove u direktorijumu
+            graph.add_edge({file, link})    #dodaje ih kao grane
+            if link in rangStranica:        #racuna koliko ce linkova pokazivati na neku html stranicu
+                rangStranica[link]+=1       #plus 1 ako se link vec nalazi u rangStranica
             else:
-                rangStranica[link] = 1  #dodaje link u rangStranica i postavlja mu rang na 1
+                rangStranica[link] = 1      #dodaje link u rangStranica i postavlja mu rang na 1
 
-            if link in imenaStranicaSaLinkovima:  #pravi novi direktorijum gde je key link a value cvorovi koji sadrze taj link
-                imenaStranicaSaLinkovima[link].append(file) #na key link dodajemo value jos jednu html stranicu
+            if link in imenaStranicaSaLinkovima:                    #pravi novi direktorijum gde je key link a value cvorovi koji sadrze taj link
+                imenaStranicaSaLinkovima[link].append(file)         #na key link dodajemo value jos jednu html stranicu
             else:
-                imenaStranicaSaLinkovima[link] = [file] #na key link dodajemo value jednu html stranicu
+                imenaStranicaSaLinkovima[link] = [file]             #na key link postavljamo vrednost niz-a i ubacujemo vrednost file
 
 
     return rangStranica, imenaStranicaSaLinkovima
